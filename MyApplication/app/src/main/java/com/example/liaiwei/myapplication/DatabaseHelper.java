910424ac8d01ad082e48cabdc8d3494057645e30
iltheeel <liaiweiwei@gmail.com>
2017-05-15 09:28:28 -0700

    DAY 3 - continue db and add address and phone number fields

--- MyApplication/app/src/main/java/com/example/liaiwei/myapplication/DatabaseHelper.java
+++ MyApplication/app/src/main/java/com/example/liaiwei/myapplication/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package com.example.liaiwei.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,6 +16,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "Contact_table";
     public static final String COL_1 = "ID";
     public static final String COL_2 = "NAME";
+    public static final String COL_3 = "ADRESS";
+
+
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -30,5 +34,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS" + TABLE_NAME);
         onCreate(db);
+    }
+
+    public boolean insertData (String name, String address, String number) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, address);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- MyApplication/app/src/main/java/com/example/liaiwei/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/liaiwei/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.liaiwei.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -9,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
     EditText editName;
+    EditText editAddress;
+    EditText editNumber;
     Button btnAddData;
 
     @Override
@@ -17,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById( R.id.editText_name);
+        editAddress= (EditText) findViewById( R.id.editText2);
+        editNumber = (EditText) findViewById(R.id.editText3);
+
+
+    }
+
+
+    public void addData(View v) {
+
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+        if(isInserted) {
+            Log.d("MyContact", "Data insertion successful");
+            //create toast message to user indicating data inserted correctly
+        }
+        else {
+            Log.d("MyContact", "Data insertion not successful");
+            //create toast message to user indicating data inserted incorrectly
+        }
     }
 }
