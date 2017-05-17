b7aa4b26fa9bfb3aa4403a2dd698978c6ed02f41
iltheeel <liaiweiwei@gmail.com>
2017-05-17 13:52:02 -0700

    (IVY LI) Day ??? - Fix string buffer to show data collected, fixed toast messages, begin search algorithm

--- MyApplication/app/src/main/java/com/example/liaiwei/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/liaiwei/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.liaiwei.myapplication;
 
+import android.app.AlertDialog;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editName;
     EditText editAddress;
     EditText editNumber;
+    EditText findName;
     Button btnAddData;
 
     @Override
@@ -27,22 +29,40 @@ public class MainActivity extends AppCompatActivity {
         editName = (EditText) findViewById( R.id.editText_name);
         editAddress= (EditText) findViewById( R.id.editText2);
         editNumber = (EditText) findViewById(R.id.editText3);
+        findName = (EditText) findViewById(R.id.nameSearch);
 
 
     }
 
+    public void searchByName(View v) {
+        Cursor res = myDb.getAllData();
+        if (res.getCount() ==0) {
+            Toast.makeText(getApplicationContext(), "DATA NOT FOUND", Toast.LENGTH_SHORT).show();
+            //put logd and toast here
+            return;
+        }
+
+
+        while(!res.isLast()){
+            if(res.getString(2).toString().equals(findName)) {
+                String result = "Person found: \n"
+                showMessage("app", )
+            }
+        }
+
+    }
 
     public void addData(View v) {
 
         boolean isInserted = myDb.insertData(editName.getText().toString(), editAddress.getText().toString(), editNumber.getText().toString());
         if(isInserted) {
             Log.d("MyContact", "Data insertion successful");
-            Toast.makeText(getApplicationContext(), "DATA INSERTION SUCCESSFUL", Toast.LENGTH_SHORT);
+            Toast.makeText(getApplicationContext(), "DATA INSERTION SUCCESSFUL", Toast.LENGTH_SHORT).show();
             //create toast message to user indicating data inserted correctly
         }
         else {
             Log.d("MyContact", "Data insertion not successful");
-            Toast.makeText(getApplicationContext(), "DATA INSERTION NOT SUCCESSFUL", Toast.LENGTH_SHORT);
+            Toast.makeText(getApplicationContext(), "DATA INSERTION NOT SUCCESSFUL", Toast.LENGTH_SHORT).show();
             //create toast message to user indicating data inserted incorrectly
         }
     }
@@ -50,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
     public void viewData(View v) {
         Cursor res = myDb.getAllData();
         if (res.getCount() ==0) {
-            showMessage("Error", "No data found in database");
+            Toast.makeText(getApplicationContext(), "DATA NOT FOUND", Toast.LENGTH_SHORT).show();
             //put logd and toast here
             return;
         }
@@ -60,18 +80,28 @@ public class MainActivity extends AppCompatActivity {
         //append each col to buffer
         while(!res.isLast()){
             res.moveToNext();
-            for(int i =0; i<4; i++) {
-                Log.d("MyContact", res.getString(i));
-                buffer.append(res.getColumnNames());
-                buffer.append(" : ");
-                buffer.append(res.getString(i));
-                buffer.append("\n");
-            }
+
+           // for(int i =0; i<4; i++) {
+            buffer.append(" \n ID : ");
+            buffer.append(res.getString(0));
+            buffer.append(" \n Name: ");
+            buffer.append(res.getString(1));
+            buffer.append(" \n Address: ");
+            buffer.append(res.getString(2));
+            buffer.append(" \n Phone Number: ");
+            buffer.append(res.getString(3));
+
         }
+        Log.d("MyContact", buffer.toString());
+
         showMessage("app", buffer.toString());
     }
 
     private void showMessage(String title, String mssg) {
-
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true); //cancel using back button
+        builder.setTitle(title);
+        builder.setMessage(mssg);
+        builder.show();
     }
 }
